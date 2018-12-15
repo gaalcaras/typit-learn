5173112a4a4fdaa7c49bdb5dbcc2a7ecb6e4b9b5
gaalcaras <gaby.alcaras@gmail.com>
2018-12-15 17:34:58 +0100

    Switch to using pynvim

--- rplugin/python3/tplearn/__init__.py
+++ rplugin/python3/tplearn/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-TypitLearn plugin for Neovim
+TypitLearn plugin for pynvim
 
 File: __init__.py
 Author: Gabriel Alcaras
@@ -8,16 +8,16 @@ License: GNU GPL v3
 """
 
 import numpy as np
-import neovim
+import pynvim
 
 from tplearn import logger
 from tplearn.manager import TypitLearnManager
 from tplearn.tracker import TypitLearnTracker
 
-@neovim.plugin
+@pynvim.plugin
 class TypitLearn(logger.LoggingMixin):
 
-    """Main plugin class to interact with Neovim"""
+    """Main plugin class to interact with pynvim"""
 
     def __init__(self, nvim):
         self.nvim = nvim
@@ -33,11 +33,11 @@ class TypitLearn(logger.LoggingMixin):
 
         self._record_first = True
 
-    @neovim.function('_typitlearn_load_abbreviations')
-    def _load_abbreviations(self, args):
+    @pynvim.function('_typitlearn_load_abbreviations')
+    def _load_abbreviations(self, *args):
         self.manager.load_abbreviations()
 
-    @neovim.rpc_export('nvim_buf_lines_event')
+    @pynvim.rpc_export('nvim_buf_lines_event')
     def _on_buf_lines_event(self, *args):
         buf = args[0]
         firstline = args[2]
@@ -59,15 +59,15 @@ class TypitLearn(logger.LoggingMixin):
                                           word_pattern)
         self.manager.show_abbrevs(self.tracker.abbrev())
 
-    @neovim.rpc_export('nvim_buf_changedtick_event')
+    @pynvim.rpc_export('nvim_buf_changedtick_event')
     def _on_buf_changedtick_event(self, *args):
         return
 
-    @neovim.rpc_export('nvim_buf_detach_event')
+    @pynvim.rpc_export('nvim_buf_detach_event')
     def _on_nvim_buf_detach_event(self, *args):
         return
 
-    @neovim.command('TypitLearnRecord', nargs=0)
+    @pynvim.command('TypitLearnRecord', nargs=0)
     def _toggle_record(self):
         if self.nvim.eval('g:tplearn_record') == 0:
             self._record_start()
@@ -76,11 +76,11 @@ class TypitLearn(logger.LoggingMixin):
             self._record_stop()
             self.nvim.vars['tplearn_record'] = 0
 
-    @neovim.command('TypitLearnEdit', nargs=0)
+    @pynvim.command('TypitLearnEdit', nargs=0)
     def _edit_abbrev_file(self):
         self.manager.edit_file()
 
-    @neovim.command('TypitLearnReload', nargs=0)
+    @pynvim.command('TypitLearnReload', nargs=0)
     def _reload(self):
         self.manager.load_abbreviations()
 
